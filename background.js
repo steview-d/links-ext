a9aeecfe867afa4e9fbc9f6252d8092357b2c21d
Dave Laffan <dave.laffan@outlook.com>
2020-02-03 15:01:21 +0000

    Remove content_scripts and use activeTab instead
    Chrome store was flagging an issue with the extension having 'Broad
    host permissions' whcih was due to matching all urls when running the
    content.js script.
    To fix, removed 'content_scripts' from the manifext, and instead using
    activeTab to inject the content.js script into only the active tab when
    the extension is clicked.

--- background.js
+++ background.js
@@ -1,5 +1,9 @@
 // Do this when the browser extension icon is clicked
-chrome.browserAction.onClicked.addListener(_ => {
+chrome.browserAction.onClicked.addListener(tab => {
+    // inject content.js into activeTab
+    chrome.tabs.executeScript(tab.id, {
+        file: "content.js"
+    });
     // Message the active tab to say the extension button has been clicked
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         let activeTab = tabs[0];
@@ -13,6 +17,8 @@ chrome.browserAction.onClicked.addListener(_ => {
 // URL using HTTP GET
 chrome.runtime.onMessage.addListener(request => {
     if (request.message === "open_links_app_with_new_url") {
-        chrome.tabs.create({ url: request.app + '/?url=' + request.destination });
+        chrome.tabs.create({
+            url: request.app + "/?url=" + request.destination
+        });
     }
 });
